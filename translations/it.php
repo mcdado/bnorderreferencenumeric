51f64c05e76f515914277e7d50eacc1cf031502e
David Gasperoni <mcdado@gmail.com>
2016-02-10 11:41:01 +0100

    Update Italian translations

--- translations/it.php
+++ translations/it.php
@@ -2,5 +2,5 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{bnorderreferencenumeric}prestashop>bnorderreferencenumeric_c2004ad0437de669156fbd0d7f3b3e30'] = 'Riferimenti ordine numerici';
-$_MODULE['<{bnorderreferencenumeric}prestashop>bnorderreferencenumeric_54b5eccc0f9b5820367cb10efcf6bc4f'] = 'Genera riferimenti ordine composti da nove cifre casuali invece che lettere maiuscole.';
+$_MODULE['<{bnorderreferencenumeric}prestashop>bnorderreferencenumeric_f20972eae641a1fcc374e73a6766c5e9'] = 'Riferimenti ordine numerici';
+$_MODULE['<{bnorderreferencenumeric}prestashop>bnorderreferencenumeric_de5353d7669f9c60c8dc990ff207b35c'] = 'Genera riferimenti ordine composti da nove cifre casuali invece che lettere maiuscole.';
