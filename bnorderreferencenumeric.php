84d130f90a83137674b1ea1489e1a304ba00944f
David Gasperoni <mcdado@gmail.com>
2018-01-15 20:46:46 +0100

    Remove superfluous overidden methods

--- bnorderreferencenumeric.php
+++ bnorderreferencenumeric.php
@@ -25,14 +25,4 @@ class BNOrderReferenceNumeric extends Module
 
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
     }
-
-    public function install()
-    {
-        return parent::install();
-    }
-
-    public function uninstall()
-    {
-        return parent::uninstall();
-    }
 }
